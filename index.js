bde2731866c23b76b4f77b6cf1718456733d84f3
ramenpepe <kwangwei@sol59.com>
2024-08-04 02:19:34 +0800

    js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const canvas = document.querySelector('canvas')
 const c = canvas.getContext('2d')
 const player1 ='dk'; //dk
 const player2 ='duo'; //dk
-canvas.width = 1024
+canvas.width = 2048
 canvas.height = 576
 
 c.fillRect(0, 0, canvas.width, canvas.height)
